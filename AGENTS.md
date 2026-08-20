ce10d660d231b3f9008ce73bd3f53d2c1980ed53
lawjiakai11 <jiakai.law@icloud.com>
2026-08-20 17:27:16 +0800

    Level-0

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: 1 year of coding experience. Have worked with java and python. Have also built a full stack web app with javascript and taskscript.
+* IDE and level of expertise: Usually use vscode, not too familiar with the intellij im using now.
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Panda.java
@@ -0,0 +1,14 @@
+/**
+ * The entry point for the Panda chatbot.
+ */
+public class Panda {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("PANDA");
+        System.out.println("Hello! I'm Panda.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
